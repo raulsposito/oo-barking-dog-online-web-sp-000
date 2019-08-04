c3e738ddbb7184ba3740bde760cc096fa61be6a3
Raul Sposito <raulsposito@gmail.com>
2019-08-04 15:46:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+  
+  def name
+    this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
+
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name 
